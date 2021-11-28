a02dfde48ff35f00f98c639d2bf7e24aeddd299a
lx2pwnd <lx2pwnd@gmail.com>
2021-11-28 14:57:24 +0100

    first commit

---
+++ Carontinho/Carontinho/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carontinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
