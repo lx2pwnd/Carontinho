2a38c910bcc93f954197b50b023c107bd3338cf3
lx2pwnd <lx2pwnd@gmail.com>
2021-11-28 17:38:06 +0100

    introduce DI

---
+++ Carontinho/Carontinho/Reader.cs
@@ -0,0 +1,32 @@
+﻿using Carontinho.Interface;
+using Microsoft.Extensions.Logging;
+using System.Configuration;
+using System.IO;
+
+namespace Carontinho
+{
+    public class Reader : IReader
+    {
+        private readonly ILogger<Reader> _logger;
+        private readonly string _path = ConfigurationManager.AppSettings["FilePath"];
+
+        public Reader(ILogger<Reader> logger)
+        {
+            _logger = logger;
+        }
+
+        public void ReadCSV()
+        {
+            _logger.LogInformation("*** Searching for files in folder *** ");
+
+            string[] fileEntries = Directory.GetFiles(_path);
+            foreach (string file in fileEntries)
+            {
+
+                var fileName = file.Substring(_path.Length + 1);
+                _logger.LogInformation($"*** Find file {fileName} ***");
+            }
+
+        }
+    }
+}
---
+++ Carontinho/Carontinho/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Carontinho.Interface;
+using Microsoft.Extensions.Logging;
+
+namespace Carontinho
+{
+    public class Startup 
+    {
+        private readonly IReader _reader;
+        private readonly ILogger<Startup> _logger;
+
+        public Startup(ILogger<Startup> logger, IReader serviceA)
+        {
+            _logger = logger;
+            _reader = serviceA;
+        }
+
+        public void Run()
+        {
+            _reader.ReadCSV();
+        }
+    }
+}
