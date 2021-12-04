3639ffb7f5edd871ddbd892d5ef7531b1a4f0550
lucentel <leonardo.lucente.external@ynap.com>
2021-12-05 00:18:01 +0100

    add writer

--- Carontinho/Carontinho/FileProcessing/CryptoCsvWriter.cs
+++ Carontinho/Carontinho/FileProcessing/CryptoCsvWriter.cs
@@ -1,9 +1,12 @@
 ﻿using Carontinho.Interface;
 using Carontinho.Model;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
 using System.IO;
 
 namespace Carontinho.FileProcessing
@@ -21,20 +24,42 @@ namespace Carontinho.FileProcessing
 
         public void WriteCSV(IDictionary<string, List<CryptoFileModel>> assetDictionary)
         {
-            CreateSubFolder();
+            var path = CreateSubFolder();
+
+            foreach (var asset in assetDictionary)
+            {
+                try
+                {
+                    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                    {
+                        Delimiter = ",",
+                    };
+                    var fileName = $"{path}\\{asset.Key}_transactions_record_{DateTime.Now.ToString("yyyy_mm_dd_hhmmss")}.csv";
+                    using (var writer = new StreamWriter(fileName))
+                    using (var csv = new CsvWriter(writer, config))
+                    {
+                        csv.WriteRecords(assetDictionary[asset.Key]);
+                    }
+                }
+                catch
+                { 
+                    _logger.LogWarning($"*** Error during the generation for the asset: {asset.Key} ***"); 
+                };
+            }
         }
 
-        private void CreateSubFolder()
+        private string CreateSubFolder()
         {
-            var folderName = DateTime.Now.ToString("yyyy_mm_dd_hhmmss");
+            var folderName = DateTime.Now.ToString("yyyy_MM_dd_hhmmss");
             var path = $"{_path}\\{folderName}";
             if (Directory.Exists(path))
             {
                 _logger.LogWarning("*** That path exists already ***");
-                return;
+                return path;
             }
 
             Directory.CreateDirectory(path);
+            return path;
         }
     }
 }
